0cc6fe1b1a0b3bd27e9a34aeb08f664e46d27080
Sangwon56 <sw000506@naver.com>
2024-07-29 15:19:32 +0900

    07/29 장상원

---
+++ mybatisapp/src/main/java/com/sbmavenweb/mybatisapp/category/CategoryDto.java
@@ -0,0 +1,15 @@
+package com.sbmavenweb.mybatisapp.category;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class CategoryDto implements ICategory {
+private Long id;
+private String name;
+}
---
+++ mybatisapp/src/main/java/com/sbmavenweb/mybatisapp/category/ICategoryService.java
@@ -0,0 +1,15 @@
+package com.sbmavenweb.mybatisapp.category;
+
+import java.util.List;
+
+public interface ICategoryService {
+
+    ICategory findById(Long id);
+    ICategory findByName(String name);
+    List<ICategory> getAllList();
+    ICategory insert(ICategory category) throws Exception;
+    Boolean delete(Long id) throws Exception;
+    ICategory update(Long id, ICategory category) throws Exception;
+    List<ICategory> findByAllNameContains(SearchCategoryDto dto);
+    int countAllByNameContains(SearchCategoryDto searchCategoryDto);
+}
