2a096c67a9d7105e17514227f9d44161ae986a5b
Sangwon56 <sw000506@naver.com>
2024-11-04 21:01:10 +0900

    add sb3stomp 대화방 목록 화면, 대화방 상세 화면 만 가능

---
+++ sb3stomp/src/test/java/com/gradle/sb3stomp/Sb3stompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradle.sb3stomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb3stompApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
